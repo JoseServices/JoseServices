c1258226c4f116d684c7815ccf3f648ffbe3b0e2
JoseServices <joseservices0011@gmail.com>
2024-11-27 23:58:07 -0400

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
+const statusMessages = ["Bot Oficial","Tu servidor, nuestra misión ⚙️","¡Adquiere tu servicio ahora! 🌟","¡Transforma tu Discord ya! ⚙️"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
